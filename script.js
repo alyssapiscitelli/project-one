8b893c6b4902f37d18e37b199977ff0a08b13f09
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-02-25 15:50:10 -0500

    project one rock paper scissors
    project rock paper scissors game

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 //Rock paper scissors game
-   var howManyRounds =[1,2,3,4,5]
-   
-   var howManyRounds =prompt('How many rounds do you want to play?');
-    
-    
+
+
+   var limitOfRounds=prompt('How many rounds do you want to play?');
+   var howManyRounds= 0;
+   while (limitOfRounds > howManyRounds)  
+   { 
     var userChoice =prompt('Rock paper or scissors?');
     console.log(userChoice);
     
     const randomNumber = Math.floor(Math.random()*2);
-    var options = ['rock', 'paper','scissor'];
-    alert(options[randomNumber]);
-    console.log(options[randomNumber]);
+
+      var options = ['rock', 'paper','scissor'];
+   
+      alert(options[randomNumber]);
+      
+      console.log(options[randomNumber]);
+
+    
 
     if(userChoice===options[randomNumber])
     {
-        alert('Ahh its a tie! Try again!');    
+        alert('Ahh its a tie! Try again!');   
     }
 
-    if(userChoice=='scissors'&& options[randomNumber]){
-        alert('Computer wins you lose!');
-
-        if(userChoice==='scissors' && options[randomNumber])
-        alert('You win scissors beats paper!');
-        if(userChoice==='paper' && options[randomNumber])
-    alert ('computer loses paper covers rock!!!!');
+   if(userChoice=='scissors'&& options[randomNumber]==='rock')
+    { 
+      alert('Computer wins you lose! Rock Crushes Scissors!');
     }
-        
-  //    if (userChoice === "paper") {
-  //    if (options === "rock") {
-  //        alert ("paper wins");
-  //    } else {
-  //        if (userChoice === "scissors") {
-  //            alert("scissors wins");
-  //        }
-  //    }
-  //    if (userChoice === "scissors") {
-  //        if (options === "rock") {
-  //            alert("rock wins");
-  //        } else {
-  //            if (options === "paper") {
-  //                alert( "scissors wins");
-  //            }
-  //        }
-  //    }
-  //}
 
+     if(userChoice==='scissors' && options[randomNumber]==='paper')
+    {
+      alert('You win scissors beats paper!');
+    }
 
+     if(userChoice=='paper' && options[randomNumber]=='rock')
+    {
+      alert ('you win paper covers rock!!!!');
+    }
+
+    if(userChoice==='rock' && options[randomNumber]==='paper')
+    {
+      alert('You lose! Computer chose paper!Paper covers rock!');
+    }
     
-
-
-  
-
+    console.log('We looped ',howManyRounds+1, ' rounds.');
+    howManyRounds++;
+  }
     
+    
+   
