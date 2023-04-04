29d2f7c0ec1cd2a261ee30778f1bb5a38342b59f
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-04-04 15:09:41 -0400

    project one update
    updated score taker

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 
    var limitOfRounds=prompt('How many rounds do you want to play?');
    var howManyRounds= 0;
+   var wins = 0;
+   var lose = 0;
+   var ties =0;
+  
    while (limitOfRounds > howManyRounds)  
    { 
     var userChoice =prompt('Rock paper or scissors?');
@@ -21,30 +25,38 @@
     if(userChoice===options[randomNumber])
     {
         alert('Ahh its a tie! Try again!');   
+        ties++;
     }
 
    if(userChoice=='scissors'&& options[randomNumber]==='rock')
     { 
       alert('Computer wins you lose! Rock Crushes Scissors!');
+      lose++;
     }
 
      if(userChoice==='scissors' && options[randomNumber]==='paper')
     {
       alert('You win scissors beats paper!');
+      wins++;
     }
 
      if(userChoice=='paper' && options[randomNumber]=='rock')
     {
       alert ('you win paper covers rock!!!!');
+      wins++;
     }
 
     if(userChoice==='rock' && options[randomNumber]==='paper')
     {
       alert('You lose! Computer chose paper!Paper covers rock!');
+      lose++;
     }
+    console.log('wins ', wins,' lose ',lose,' ties ',ties);
+
     
     console.log('We looped ',howManyRounds+1, ' rounds.');
     howManyRounds++;
+
   }
     
     
